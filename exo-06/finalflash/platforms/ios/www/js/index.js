fb258be1581c0fa92fdef84da20e7c20fb70d0b7
Bachelor 3 <bachelor3@LATtop.fro-204.froment.in.iesanetwork.com>
2017-04-21 10:01:41 +0200

    List contact / Add contact

--- exo-06/finalflash/platforms/ios/www/js/index.js
+++ exo-06/finalflash/platforms/ios/www/js/index.js
@@ -18,57 +18,94 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
+initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+},
+    
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+    
+    
+    
+    function checkConnection() {
+        var networkState = navigator.connection.type;
         
-        var deviceVesion = document.getElementById('device');
-        deviceVesion.innerHTML = '<ul data-role="listview" id="list"'+
-        '<li data-icon="arrow-l">Name:' + device.manufacturer + '</li>'+
-        '<li data-icon="arrow-r">Model:' + device.model + '</li>'+
-        '<li data-icon="arrow-r">Cordova:' + device.cordova + '</li>'+
-        '<li data-icon="arrow-l">Platform:' + device.platform + '</li>'+
-        '<li data-icon="arrow-r">Uuid:' + device.uuid + '</li>'+
-        '<li data-icon="arrow-l">Version:' + device.version + '</li>'+
-        '</ul>';
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
         
-        function checkConnection() {
-            var networkState = navigator.connection.type;
-            
-            var states = {};
-            states[Connection.UNKNOWN]  = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI]     = 'WiFi connection';
-            states[Connection.CELL_2G]  = 'Cell 2G connection';
-            states[Connection.CELL_3G]  = 'Cell 3G connection';
-            states[Connection.CELL_4G]  = 'Cell 4G connection';
-            states[Connection.CELL]     = 'Cell generic connection';
-            states[Connection.NONE]     = 'No network connection';
-            
-            alert('Connection type: ' + states[networkState]);
-        }
-        
-        checkConnection();
+        alert('Connection type: ' + states[networkState]);
     }
+    
+    checkConnection();
+    
+    /*function useStorage(){
+     var oldConn = window.getItem("old");
+     document.getElementById('oldDate').innerHTML = oldConn;
+     
+     navigator.app.exitApp();
+     
+     window.setItem
+     
+     
+     }*/
+    var myContact = navigator.contacts.create({"displayName": "Bejita LOL"});
+    myContact.save;
+    
+    function onSuccess(contacts) {
+        alert('Found ' + contacts.length + ' contacts.');
+        var contactDiv = document.getElementById('contactList');
+        var listContact = "";
+        for(var i=0; i< contacts.length; i++){
+            listContact += "<li>"+contacts[i].name.givenName+"</li>";
+        }
+        contactDiv.innerHTML = "<ul>"+listContact+"</ul>";
+    };
+    
+    function onError(contactError) {
+        alert('onError!');
+    };
+    var options      = new ContactFindOptions();
+    options.multiple = true;
+    var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+    navigator.contacts.find(fields, onSuccess, onError, options);
+  
+    
+    
+
+},
+    
+    // Update DOM on a Received Event
+receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    
+    /*var deviceVesion = document.getElementById('device');
+    deviceVesion.innerHTML = '<ul data-role="listview" id="list"'+
+    '<li data-icon="arrow-l">Name:' + device.manufacturer + '</li>'+
+    '<li data-icon="arrow-r">Model:' + device.model + '</li>'+
+    '<li data-icon="arrow-r">Cordova:' + device.cordova + '</li>'+
+    '<li data-icon="arrow-l">Platform:' + device.platform + '</li>'+
+    '<li data-icon="arrow-r">Uuid:' + device.uuid + '</li>'+
+    '<li data-icon="arrow-l">Version:' + device.version + '</li>'+
+    '</ul>';*/
+    
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+    
+    console.log('Received Event: ' + id);
+}
 };
 
 app.initialize();
